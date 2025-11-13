c7eabfa92100530b343dcccc2902993fb6dbe34b
RUNI-Harel <harel.milo@post.runi.ac.il>
2025-11-13 18:50:42 +0200

    1st attempt

--- Anagram.java
+++ Anagram.java
@@ -2,18 +2,27 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		System.out.println(isAnagram("silent", "listen")); // true
+		// System.out.println(isAnagram("William Shakespeare", "I am a weakish
+		// speller")); // true
+		// System.out.println(isAnagram("Madam Curie", "Radium came")); // true
+		// System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort"));
+		// // true
 
 		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
+		//System.out.println(preProcess("What? No way!!!"));
+		//System.out.println(isAnagram("abcecbe", "ebcecba"));
+		System.out.println(isAnagram("c-b-a", "a?bc?"));
+		//System.out.println(isAnagram("uhygtd76m", "hvghrnvd"));
+		//System.out.println(randomAnagram("harEl"));
+		System.out.println(isAnagram("hello", "world!"));
+
 		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
+		// System.out.println("silent and " + randomAnagram("silent") + " are
+		// anagrams.");
+		System.out.println(randomAnagram("abcdefg"));
+
+		// Performs a stress test of randomAnagram
 		String str = "1234567";
 		Boolean pass = true;
 		//// 10 can be changed to much larger values, like 1000
@@ -21,29 +30,98 @@ public class Anagram {
 			String randomAnagram = randomAnagram(str);
 			System.out.println(randomAnagram);
 			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
+			if (!pass)
+				break;
 		}
 		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+	}
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
 		// Replace the following statement with your code
-		return false;
+		String newStr1 = preProcess(str1);
+		String newStr2 = preProcess(str2);
+		char s1 = 'z';
+		char s2 = 'x';
+		//remove spaces
+		String regexString = "\\s";
+		newStr1=newStr1.replaceAll(regexString, "");
+		newStr2=newStr2.replaceAll(regexString, "");
+		int index=0;
+		if((newStr1 == "") || (newStr2 == "")){
+			return true;
+		}
+		if (newStr1.length() != newStr2.length()) {
+			return false;
+		} else {
+			while (newStr1.length()>0) {
+				s1 = newStr1.charAt(0);
+				for (int j = 0; j < newStr2.length(); j++) {
+					s2 = newStr2.charAt(j);
+					if (s1 == s2) {
+						newStr1 = newStr1.substring(1);
+						newStr2 = newStr2.substring(0, j) + newStr2.substring(j + 1);
+						index++;
+						break;
+					}
+					if (j == newStr2.length()-1 && s1!=s2) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+
 	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
+
+	// Returns true if the two given strings are anagrams, false otherwise.
+	/*
+	 * public static boolean isAnagram(String str1, String str2) {
+	 * // Replace the following statement with your code
+	 * String newStr1 = preProcess(str1);
+	 * String newStr2 = preProcess(str2);
+	 * if (newStr1.length() != newStr2.length() || (newStr1 == "") || (newStr2 ==
+	 * "")) {
+	 * return false;
+	 * } else {
+	 * for (int i = 0; i < newStr1.length() - 1; i++) {
+	 * if (newStr1.charAt(i) != newStr2.charAt(newStr2.length() - 1 - i)) {
+	 * return false;
+	 * }
+	 * }
+	 * }
+	 * return true;
+	 * 
+	 * }
+	 */
+
+	// Returns a preprocessed version of the given string: all the letter characters
+	// are converted
+	// to lower-case, and all the other characters are deleted, except for spaces,
+	// which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
-	} 
-	   
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
+		// this is my code
+		String regexString = "[`!@#$%^&*(){};\\':\",./<>?\\[\\]=-]";
+		String newStr = str.toLowerCase();
+		newStr = newStr.replaceAll(regexString, "");
+		return newStr;
+	}
+
+	// Returns a random anagram of the given string. The random anagram consists of
+	// the same
+	// characters as the given string, re-arranged in a random order.
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
+		String procStr = preProcess(str);
+		String newStr = "";
+		int procStrLen = procStr.length();
+		for (int i = 0; i < procStrLen; i++) {
+			int rand = (int) (Math.random() * procStr.length() - 1);
+			newStr = newStr + procStr.charAt(rand);
+			procStr = procStr.substring(0, rand) + procStr.substring(rand + 1);
+		}
+		return newStr;
 	}
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -1,13 +1,13 @@
 // Computes the periodical payment necessary to pay a given loan.
 public class LoanCalc {
-	
-	static double epsilon = 0.001;  // Approximation accuracy
-	static int iterationCounter;    // Number of iterations 
-	
+
+	static double epsilon = 0.001; // Approximation accuracy
+	static int iterationCounter; // Number of iterations
+
 	// Gets the loan data and computes the periodical payment.
-    // Expects to get three command-line arguments: loan amount (double),
-    // interest rate (double, as a percentage), and number of payments (int).  
-	public static void main(String[] args) {		
+	// Expects to get three command-line arguments: loan amount (double),
+	// interest rate (double, as a percentage), and number of payments (int).
+	public static void main(String[] args) {
 		// Gets the loan data
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
@@ -15,9 +15,9 @@ public class LoanCalc {
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
 		// Computes the periodical payment using brute force search
-		System.out.print("\nPeriodical payment, using brute force: ");
-		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
-		System.out.println("number of iterations: " + iterationCounter);
+		//System.out.print("\nPeriodical payment, using brute force: ");
+		//System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
+		//System.out.println("number of iterations: " + iterationCounter);
 
 		// Computes the periodical payment using bisection search
 		System.out.print("\nPeriodical payment, using bi-section search: ");
@@ -26,29 +26,76 @@ public class LoanCalc {
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
-	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	private static double endBalance(double loan, double rate, int n, double payment) {	
+	// interest rate (as a percentage), the number of periods (n), and the
+	// periodical payment.
+	private static double endBalance(double loan, double rate, int n, double payment) {
 		// Replace the following statement with your code
-		return 0;
+		double loanTwo=loan;
+		for (int i = 0; i < n; i++) {
+			loanTwo = loanTwo * rate;
+			loanTwo = loanTwo - payment;
+			//System.out.println("New loan: " + loanTwo);
+
+		}
+		return loanTwo; //thats whats left
 	}
-	
+
 	// Uses sequential search to compute an approximation of the periodical payment
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
 		// Replace the following statement with your code
-		return 0;
-    }
-    
-    // Uses bisection search to compute an approximation of the periodical payment 
+		double g = (loan / n);
+		iterationCounter = 0;
+		double newLoan = loan;
+		while (newLoan > 0) {
+			newLoan = loan;
+			iterationCounter++;
+			// System.out.println("G: " + g);
+			for (int i = 0; i < n; i++) {
+				newLoan = newLoan * rate;
+				// System.out.println("New loan: " + newLoan);
+				newLoan = newLoan - g;
+				System.out.println("New loan: " + newLoan);
+			}
+			g = g + 0.01;
+		}
+		System.out.println(g - 0.01);
+		System.out.println("Whats left: " + endBalance(loan,rate,n,g));
+		return g;
+	}
+
+	// Uses bisection search to compute an approximation of the periodical payment
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
-    }
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		// Replace the following statement with your code
+		//TODO: Fix all this
+		double l = (loan / n);
+		double h = loan;
+		double g = (l+h)/2;
+		iterationCounter = 0;
+		double newLoan = loan;
+		while (h-l>0.1) {
+			for (int i = 0; i < n; i++) {
+				newLoan = newLoan * rate;
+				// System.out.println("New loan: " + newLoan);
+				newLoan = newLoan - g;
+				System.out.println("New loan: " + newLoan);
+			}
+			if (newLoan>0) {
+				l=g;
+			}else{
+				h=g;
+			}
+			g=(h+l)/2;
+			newLoan=loan;
+			iterationCounter++;
+		}
+		return g;
+	}
 }
